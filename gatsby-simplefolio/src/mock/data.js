0ee82691c612cb7c731c796332e3665301660076
Natalie Murphy <Nataliemurphy500@gmail.com>
2021-01-11 14:59:12 -0800

    adds content to projects

--- gatsby-simplefolio/src/mock/data.js
+++ gatsby-simplefolio/src/mock/data.js
@@ -11,55 +11,63 @@ export const headData = {
 export const heroData = {
   title: '',
   name: 'Natalie Murphy',
-  subtitle: 'Web Developer',
+  subtitle: 'I am a Web Developer',
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'shore.jpg',
+  paragraphOne:
+    'Currently, I am starting my internship after completing Epicodus, an awesome web development bootcamp based in Portland OR. Excited to build a career in technology and grow with the ever-changing times and technologies.',
+  paragraphTwo:
+    'While I am new to this industry, I am proactive with research on subjects that I do not understand. I value mentorship, as I have been a mentor to students in the lower cohort at Epicodus. The community has been what keeps me excited about programming. In my opinion, collaboration leads to more successful projects. Working with my colleagues has been a highlight during my time at Epicodus.',
+  paragraphThree:
+    'In other news, if you love coffee and skiing I think we will have a great time working together. ',
+  resume: 'https://murphynd.github.io/resume/', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'thump.png',
+    title: 'Thump',
+    info: 'Technology used: Tone.js, React.js, Vercel',
+    info2:
+      'Epicodus Capstone Project - I created a 808 inspired Online Drum Machine! It can sequence 16 notes with 5 instruments. Built in React and powered by Tone.js.',
+    url: 'https://thump-dlbpma8oe.vercel.app/',
+    repo: 'https://github.com/murphynd/Capstone', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'API.png',
+    title: 'Parks Lookup API',
+    info:
+      'Technology used: C#, ASP.NET Core MVC, MySQL, Entity Framework Core 2.2.6, Swagger - NSwag 13.3.0, Postman',
+    info2:
+      'An API that functions as catalog for National & State Parks in the USA. It utilizes RESTful principles, Swagger and has integrated authentication via JWT Authentication to keep the API Read-Only for all users except administrators.',
+    url: 'https://github.com/murphynd/ParksLookUp',
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'Auklo.png',
+    title: 'Auklo',
+    info:
+      'Technology used: C#/Javascript, WebAudio, ASP.NET Core MVC, MySQL, Entity Framework Core 2.2.6,',
+    info2:
+      'Procedurally generated ambient music maker that has an infinite timeline of sound built using C#/Javascript/MySql. Using different filters and effects, You are able to create and save your own ambient music.',
+    url: '', // rebuild in react with firebase
+    repo: 'https://github.com/murphynd/AudioProject', // if no repo, the button will not show up
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: 'Drop me a line',
+  btn: 'Email',
+  email: 'Hello@natpdx.com',
 };
 
 // FOOTER DATA
@@ -68,22 +76,22 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: '',
+      url: 'https://twitter.com/Nat_Codes_Stuff',
     },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
+    // {
+    //   id: nanoid(),
+    //   name: 'codepen',
+    //   url: '',
+    // },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/nataliedoraismurphy/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/murphynd',
     },
   ],
 };
